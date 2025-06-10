375a789aea4e479ef31d687474e29d94aa1d229f
Jean Paul <saopaulojean74@gmail.com>
2025-06-10 00:15:16 -0300

    Acrescentando models

--- contact/models.py
+++ contact/models.py
@@ -10,3 +10,6 @@ class Contact(models.Model):
     email = models.EmailField(max_length=50, blank=True)
     created_data = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
