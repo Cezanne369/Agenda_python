e2af4752e75d35251b22f9556836bdaff9eff2f2
Jean Paul <saopaulojean74@gmail.com>
2025-06-11 03:08:00 -0300

    Acrescentando nomes dos 'cliente' na home

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from contact.models import Contact
+
 
 # Create your views here.
 def index(request): # type: ignore
+    contacts = Contact.objects.all()
+
+    context = {
+        'contacts': contacts,
+    }   
     return render(
         request, # type: ignore
         'contact/index.html',
+        context,
     )
