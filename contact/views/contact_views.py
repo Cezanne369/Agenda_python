cd7ebaf5c29f2b8faa016b240b3b421edb2dff70
Jean Paul <saopaulojean74@gmail.com>
2025-06-12 02:23:02 -0300

    add links e barra de pesquisa

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,16 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
 from contact.models import Contact
 
 
-# Create your views here.
-def index(request): # type: ignore
-    contacts = Contact.objects.all()
+def index(request):# type:ignore
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .order_by('-id')[10:20]
 
     context = {
         'contacts': contacts,
-    }   
+    }
+
     return render(
-        request, # type: ignore
+        request,# type:ignore
         'contact/index.html',
-        context,
+        context
+    )
+
+
+def contact(request, contact_id):# type:ignore
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    single_contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+
+    context = {
+        'contact': single_contact,
+    }
+
+    return render(
+        request,# type:ignore
+        'contact/contact.html',
+        context
     )
