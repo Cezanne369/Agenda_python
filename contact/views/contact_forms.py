4e5d380ee48aa482cdd80611911c95c7201499f9
Jean Paul <saopaulojean74@gmail.com>
2025-06-14 00:53:55 -0300

    criando paagina de create contact

---
+++ contact/views/contact_forms.py
@@ -0,0 +1,21 @@
+#type:ignore
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
+
+from contact.models import Contact
+
+
+def create(request):
+    if request.method == 'POST':
+        request.POST.get('first name')
+        request.POST.get('last name')
+    context = {
+
+    }
+
+    return render(
+        request,
+        'contact/create.html',
+        context
+    )
