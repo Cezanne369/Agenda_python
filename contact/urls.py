7d98e54cf4f8bb979ddd3c7d7337d02d3522283d
Jean Paul <saopaulojean74@gmail.com>
2025-06-15 00:11:11 -0300

    opção para update

--- contact/urls.py
+++ contact/urls.py
@@ -1,4 +1,4 @@
-#type:ignore
+# type:ignore
 from django.urls import path
 
 from contact import views
@@ -6,12 +6,12 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
-   
-    path('search/', views.search, name='search'),  # type:ignore
-    path('', views.index, name='index'),  # type:ignore
+    path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
 
-     path('contact/<int:contact_id>/detail/', views.contact, name='contact'),  # type:ignore
-     path('contact/create/', views.create, name='create'),  # type:ignore
-     path('contact/<int:contact_id>/update/', views.update, name='update'),  # type:ignore
-     path('contact/<int:contact_id>/delete/', views.contact, name='contact'),  # type:ignore
+    # contact (CRUD)
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
 ]
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,17 +1,17 @@
 #type:ignore
-from django.shortcuts import render, redirect, get_list_or_404
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
-
 
 from contact.forms import ContactForm
 from contact.models import Contact
+
 
 def create(request):
     form_action = reverse('contact:create')
 
     if request.method == 'POST':
-        form: ContactForm(request.POST)
-        
+        form = ContactForm(request.POST, request.FILES)
+
         context = {
             'form': form,
             'form_action': form_action,
@@ -39,13 +39,15 @@ def create(request):
     )
 
 
-def update(request,contact_id):
-    contact = get_list_or_404(Contact, pk=contact_id, show=True)
+def update(request, contact_id):
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
     form_action = reverse('contact:update', args=(contact_id,))
 
     if request.method == 'POST':
-        form: ContactForm(request.POST, instance=contact)
-        
+        form = ContactForm(request.POST,request.FILES, instance=contact)
+
         context = {
             'form': form,
             'form_action': form_action,
@@ -62,7 +64,7 @@ def update(request,contact_id):
         )
 
     context = {
-        'form': ContactForm(),
+        'form': ContactForm(instance=contact),
         'form_action': form_action,
     }
 
@@ -70,4 +72,24 @@ def update(request,contact_id):
         request,
         'contact/create.html',
         context
+    )
+
+
+def delete(request, contact_id):
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+    confirmation = request.POST.get('confirmation', 'no')
+
+    if confirmation == 'yes':
+        contact.delete()
+        return redirect('contact:index')
+
+    return render(
+        request,
+        'contact/contact.html',
+        {
+            'contact': contact,
+            'confirmation': confirmation,
+        }
     )
